8a67f5238bd92ecb8cbe993f64fc721e80650761
MKinanA <mkinanahsan@gmail.com>
2024-09-06 14:23:30 +0700

    Now uses device_preview, and cleaned unneeded things (flutter clean)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:buku_bacaan_wajib_untuk_mahasiswa/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const App());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => const App(),
+    defaultDevice: Devices.android.smallPhone
+  ));
 }
 
 class App extends StatelessWidget {
@@ -11,6 +16,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         brightness: MediaQuery.of(context).platformBrightness
       ),
